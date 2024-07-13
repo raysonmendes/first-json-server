15eb11ac27a2d9d2dd3ea9d1df94b399fc3c3928
Rayson N Mendes <raysonmendes2.0@gmail.com>
2024-07-13 18:04:02 -0300

    chore: second attempt

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
-
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
+    "/product/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
