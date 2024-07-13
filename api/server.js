c23a0eb2a4dd39908dca3c756e0f909bccc63834
Rayson N Mendes <raysonmendes2.0@gmail.com>
2024-07-13 18:59:39 -0300

    chore: third attempt

--- api/server.js
+++ api/server.js
@@ -1,15 +1,11 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/product/:resource/:id/show": "/:resource/:id",
-  })
-);
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
